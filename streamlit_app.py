26f762b5ff4ad9aaf16d466220c72c74c76b5b66
Chris10424 <christian.brunnenberg@gmail.com>
2024-07-03 07:59:35 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,22 @@
 import streamlit as st
 import pandas as pd
 import mag4 as mg
+import matplotlib.pyplot as plt
 
-st.write(mg.available_datasets())
-
+data = mg.available_datasets()
+st.write(data)
+#df = mg.get_data('Bastar Craton')
+#st.write(df)
 st.write('Georoc Data')
-st.selectbox('Name', [1,2,3])
+sel_data = st.selectbox('Name', data['Title'])
+st.write(sel_data)
+test = mg.get_data(sel_data)
+
+Elemente = test.columns.tolist()[27:]
+El1= st.selectbox('x-Achse', Elemente )
+El2= st.selectbox('y-Achse', Elemente)
+
+plt.scatter(test[El1], test[El2])
+st.pyplot(plt)
+
+
